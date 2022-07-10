37f1d948332caac5b70ed8ae78227ce5c12f9a8d
joe-white09 <joe_white09@yahoo.com>
2022-07-09 22:07:29 -0500

    employee route now combines all necessary data

--- routes/apiRoutes/employeeRoleRoutes.js
+++ routes/apiRoutes/employeeRoleRoutes.js
@@ -4,9 +4,14 @@ const db = require('../../db/connection');
 
 // get all roles
 router.get('/role', (req, res) => {
-    const sql = `SELECT employee_role.title, employee_role.id as role_id, employee_role.salary, department.department_name as department_name
-    FROM employee_role
-    JOIN department ON employee_role.department_id = department.id;`;
+    const sql = `SELECT
+                employee_role.title, 
+                employee_role.id as role_id, 
+                employee_role.salary, 
+                departments.department_name as department_name
+                FROM employee_role
+                JOIN departments ON employee_role.department_id = departments.id;`;
+                
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
